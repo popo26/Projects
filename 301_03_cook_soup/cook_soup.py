93153dd5fb004b7e57d8da3c2d86412474486461
popo26 <mynameispopo26@gmail.com>
2022-03-28 15:59:30 +1300

    28march2022:afternoon commit

--- 301_03_cook_soup/cook_soup.py
+++ 301_03_cook_soup/cook_soup.py
@@ -9,54 +9,45 @@ load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
 
+ingredient_list = {"potato": 2, "tomato": 3}
+# ingredient_list = {}
+# while True:
+#     ingredient = input("what ingredients would you like to use?: ")
+#     amount = int(input("how much? : "))
+#     ingredient_list[ingredient] = amount
+#     no_more = input("Do you have more to add? : ").lower()
+#     if no_more == "no":
+#         break
+
 class Ingredient():
-    def __init__(self,**amount):
-        self.amount = amount
+    def __init__(self,**kwargs):
+        """Ingredient list should be a dictionary with key value pair. 
+        key:ingredient name as str
+        value: amount as numeric     """   
+        self.ingredient_list = kwargs
 
-    # def __str__(self):
-    #     return f"Your ingredients are {[k for k in self.amount.keys()]}. Amount is {[v for v in self.amount.values()]}"
-
-class Spice(Ingredient):
-    ### EXTENDED TASK 1:
-    # take the amount of each Ingredient() into account. How can you work that into creating a recipe the user can actually cook?
-    # Ai firstly used *name only to take ingredient names without amount(However without *(args) I could still take in multiple ingredients. So not sure what is the benefit of using *(args)here). Then i, ntroduced **amount, noticed I don't need *name since **amount covers both ingredient names and quantity so I removed *name.
-    ###
-    def __init__(self, **amount):
-        super().__init__(amount=amount)
-        self.spice_amount = amount
-        self.meal_titles = []
-        self.source_urls = []
-        self.dictionary = {}
-        self.better_ing_list = []
-        self.recipe_ids = []
-    
     def __str__(self):
-        return f"Your spices are {[k for k in self.spice_amount.keys()]}. Amount is {[v for v in self.spice_amount.values()]}"
+        return f"Your ingredients are {list(self.ingredient_list.keys())}. Amount is {list(self.ingredient_list.values())}"
 
-class Soup(Spice):
-    def __init__(self, **amount):
-        super().__init__(amount=amount) 
-             
-        self.amount = input("\nWhat ingredients would you like to use?\nType name=number separated by commas.\ne.g. 'potato=3': ")
-        # print(type(self.amount))  --> I thought kwargs will create a dictionary but it is creating a string
+class Soup(Ingredient):
+    """ EXTENDED TASK 1:
+      take the amount of each Ingredient() into account. How can you work that into creating a recipe the user can actually cook?
+    """
 
-        #Create a dictionary of ingredients(keys) and amount(values)
-        try:
-            self.dictionary = dict((x.strip(), y.strip()) for x, y in (element.split('=') for element in self.amount.split(', ')))
-        except ValueError:
-            (print("\nPlease make sure the ingredient entry is like this: e.g.potato=2"))
-
-        #Create a list of keys(ingredients)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs) 
         self.recipe_ids = []
         self.ingredients_list = []
         self.missing_ing_lines = []
-        for k in self.dictionary.keys():
-            self.ingredients_list.append(k)
+        self.meal_titles = []
+        self.better_ing_list = []
+        self.source_urls = []
 
     def cook(self):
-        ###EXTENDED TASK2:
-        # interface with a recipe API instead of concatenating a web search URL. Receive the API response data, format it, and display it as command-line output.
-        ###
+        """EXTENDED TASK2:
+        interface with a recipe API instead of concatenating a web search URL. 
+        Receive the API response data, format it, and display it as command-line output."""
+        
         # spoonacular_url = f"https://api.spoonacular.com/recipes/findByIngredients?apiKey={API_KEY}&ingredients={self.ingredients_list}&number=3"
         # response = requests.get(spoonacular_url)
         # data = response.json()
@@ -114,8 +105,6 @@ class Soup(Spice):
                     qua_unit.append(u)
                 each_menu.append(qua_unit)
             self.required_quantity_used_ingredients.append(each_menu)
-        # print('\nThe ingredients user has VS the required quantity for this recipe:')
-        # print(self.required_quantity_used_ingredients)
 
         #For readability for user
         for menu in self.required_quantity_used_ingredients:
@@ -146,12 +135,26 @@ class Soup(Spice):
        
     def __str__(self):
         backslash_n = '\n'
-        return f"\nYour ingredients are {[k for k in self.dictionary.keys()]}.\nAmount is {[v for v in self.dictionary.values()]}.\n\nMy 3 suggestions for recipes are: \n(1){self.meal_titles[0]}***🥄***{self.source_urls[0]}\n**********The required quanty of ingredients you currently have are **********\n**********{[item for item in self.better_ing_list[0]]}\n\{self.missing_ing_lines[0]}\n\n(2){self.meal_titles[1]}***🥄***{self.source_urls[1]}\n**********The required quanty of ingredients you currently have are **********\n**********{[item for item in self.better_ing_list[1]]}\n{self.missing_ing_lines[1]}\n\n(3){self.meal_titles[2]}***🥄***{self.source_urls[2]}\n**********The required quanty of ingredients you currently have are **********\n**********{[item for item in self.better_ing_list[2]]}\n{self.missing_ing_lines[0]}\n\n"
+        return f"""\nYour ingredients are {list(self.ingredient_list.keys())}.
+        \nAmount is {list(self.ingredient_list.values())}.
+        \n\nMy 3 suggestions for recipes are: 
+        \n(1){self.meal_titles[0]}***🥄***{self.source_urls[0]}
+        \n**********The required quanty of ingredients you currently have are **********
+        \n**********{[item for item in self.better_ing_list[0]]}
+        \n\{self.missing_ing_lines[0]}
+        \n\n(2){self.meal_titles[1]}***🥄***{self.source_urls[1]}
+        \n**********The required quanty of ingredients you currently have are **********
+        \n**********{[item for item in self.better_ing_list[1]]}
+        \n{self.missing_ing_lines[1]}
+        \n\n(3){self.meal_titles[2]}***🥄***{self.source_urls[2]}
+        \n**********The required quanty of ingredients you currently have are **********
+        \n**********{[item for item in self.better_ing_list[2]]}
+        \n{self.missing_ing_lines[0]}
+        \n\n"""
 
 
-
-###TASK3:
-#create child classes to Soup() that have specific qualities.
+"""TASK3:
+create child classes to Soup() that have specific qualities."""
 
 class Information(Soup):
 
@@ -187,6 +190,7 @@ class Information(Soup):
                 "fat": "",
                 "protein": "",
             }
+
         for i in range(len(self.recipe_ids)):
             # spoonacular_nutrition_url = f"https://api.spoonacular.com/recipes/{self.recipe_ids[i]}/nutritionWidget.json?apiKey={API_KEY}"
             # response = requests.get(spoonacular_nutrition_url)
@@ -205,7 +209,18 @@ class Information(Soup):
             calory_info_dict["protein"] = data["protein"]
             calory_info_list.append(calory_info_dict)
 
-        print(f"\n\n{self.meal_titles[0]} info:\n==========Total Calories: {calory_info_list[0]['calories']}==========\n==========Total Fat: {calory_info_list[0]['fat']}==========\n==========Total Protein: {calory_info_list[0]['protein']}==========.\n\n{self.meal_titles[1]} info:\n==========Total Calories: {calory_info_list[1]['calories']}==========\n==========Total Fat: {calory_info_list[1]['fat']}==========\n==========Total Protein: {calory_info_list[1]['protein']}==========.\n\n{self.meal_titles[2]} info:\n==========Total Calories: {calory_info_list[2]['calories']}==========\n==========Total Fat: {calory_info_list[2]['fat']}==========\n==========Total Protein: {calory_info_list[2]['protein']}==========.")
+        print(f"""\n\n{self.meal_titles[0]} info:
+                  \n==========Total Calories: {calory_info_list[0]['calories']}==========
+                  \n==========Total Fat: {calory_info_list[0]['fat']}==========
+                  \n==========Total Protein: {calory_info_list[0]['protein']}==========.
+                  \n\n{self.meal_titles[1]} info:
+                  \n==========Total Calories: {calory_info_list[1]['calories']}==========
+                  \n==========Total Fat: {calory_info_list[1]['fat']}==========
+                  \n==========Total Protein: {calory_info_list[1]['protein']}==========.
+                  \n\n{self.meal_titles[2]} info:
+                  \n==========Total Calories: {calory_info_list[2]['calories']}==========
+                  \n==========Total Fat: {calory_info_list[2]['fat']}==========
+                  \n==========Total Protein: {calory_info_list[2]['protein']}==========.""")
 
     def gluten(self):
         # super().cook()
@@ -254,25 +269,14 @@ class Information(Soup):
             gulten_dict["gulten_free"] = gulten_info
             self.gultenfree_infos.append(gulten_dict)
 
-        print(f"\n\n{self.meal_titles[0]} has gulten: {self.gultenfree_infos[0]['gulten_free']}.\n{self.meal_titles[1]} has gulten: {self.gultenfree_infos[1]['gulten_free']}.\n{self.meal_titles[2]} has gulten: {self.gultenfree_infos[2]['gulten_free']}.")
+        print(f"""\n\n{self.meal_titles[0]} has gulten: {self.gultenfree_infos[0]['gulten_free']}.
+                  \n{self.meal_titles[1]} has gulten: {self.gultenfree_infos[1]['gulten_free']}.
+                  \n{self.meal_titles[2]} has gulten: {self.gultenfree_infos[2]['gulten_free']}.""")
 
-# soup = Soup()
-# soup.cook()
-# print(soup)
+soup = Soup(**ingredient_list)
+soup.cook()
+print(soup)
 
 info = Information()
-info.cook()
-print(info)
-
 info.calory()
-# #time.sleep(3)
 info.gluten() 
-
-
-##Work in Progress, 2 parent classes have no meaning.
-#If I dont add (self.name and) self.spice_name - due to MRO, before it gets self.spice_name it goes to API link and erros due to not able to find self.spice_name since it goes to cook() before get the attribute
-#Feel like not fully understood args and kwargs.self.name works without *name.
-
-#if daily 150 points ran out. Try again tomorrow --> Ai added json dump
-
-
