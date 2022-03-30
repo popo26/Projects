30f48aca4e7a613a59e557b0c0ce86ea9ebebaf7
popo26 <mynameispopo26@gmail.com>
2022-03-30 14:46:34 +1300

    30march2022: build_a_card_game in progress

--- 301_03_build_a_card_game/card_game.py
+++ 301_03_build_a_card_game/card_game.py
@@ -1,13 +1,35 @@
-#Idea: https://ehmatthes.github.io/pcc_2e/challenges/playing_cards/
+#Old Maid
 
-from cards import Joker, Club, Heart, Diamond, Spade
+from re import I
+from cards import Player, PlayGame
 
-club = Club()
-heart = Heart()
-diamond = Diamond()
-spade = Spade()
+num_players = int(input("How many players?: "))
 
-print(club)
-print(heart)
-print(diamond)
-print(spade)
+player_list = []
+for i in range(0, num_players):
+    name = f"player{i}"
+    x = Player(name)
+    player_list.append(x)
+print(player_list)
+
+# player = Player("Player1")
+# print(player)
+# player.player_list()
+
+game = PlayGame(num_players)
+game.shuffle_cards()
+
+game.split_cards(num_players)
+
+# players_cards = {}
+# for player in player_list:
+#     for cards in game.final_all_cards:
+#         players_cards[player] = cards
+# print(players_cards)
+
+game.allocate_cards(*player_list)
+
+
+
+
+
--- 301_03_build_a_card_game/cards.py
+++ 301_03_build_a_card_game/cards.py
@@ -1,26 +1,62 @@
 import random
 
-class Joker():
-    def __init__(self):
-        pass
+class Player:
+    def __init__(self, name):
+        self.name = name
 
-class Club():
-    def __init__(self):
-        self.num = {"club": random.randint(1,14)}
-        print(self.num)
+    def __str__(self):
+        return self.name
 
-    # def __str__(self):
-    #     return f"This card is {self.num.keys()} {self.num.values()}"
 
-class Diamond(Club):
-    def __init__(self):
-        super().__init__()
-        self.num = {"diamond": random.randint(1,14)}
+class PlayGame:
+    def __init__(self, num_players):
+        self.cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "K" ]
+        self.num_players = num_players
+        self.final_all_cards = []
 
-class Heart():
-    def __init__(self):
-        pass
+    def shuffle_cards(self):
+        random.shuffle(self.cards)
+        print(self.cards)
 
-class Spade():
-    def __init__(self):
-        pass
+    def split_cards(self, num_players):
+        step = 51 // num_players
+        # print(step)
+        all_cards = []
+        for i in range(0, len(self.cards), step):
+            chunk = []
+            chunk.append(self.cards[i:i+step])
+            all_cards.append(chunk)
+
+        if len(all_cards) > num_players:
+            last_chunk = all_cards.pop(-1)[0]
+            for i in range(len(last_chunk)):
+                for index in range(len(all_cards)):
+                    if last_chunk == []:
+                        break
+                    else:
+                        all_cards[index][0].append(last_chunk[0])
+                        last_chunk.remove(last_chunk[0])
+
+        self.final_all_cards = []
+        for i in range(len(all_cards)):
+            self.final_all_cards.append(all_cards[i][0])
+        print(self.final_all_cards)
+
+    def allocate_cards(self, *args):
+        players_cards = {}
+        for player in args:
+            for cards in self.final_all_cards:
+                players_cards[player] = cards
+        print(players_cards)
+
+    def pick_card(self, player_num):
+        print("Pick a card from next player.")
+        # players_cards = 
+
+        
+# if __name__ == "__main__":
+#     player_list = Player()
+#     num_player = Player()
+
+
+
